d3e57c4b96e0559277da51462329ce8b051f732c
Wes <gkm85663@gmail.com>
2020-08-09 22:52:32 +0800

    Add image and create new object - "DollarSign"

--- Assets/Practice2/ObjectMoving.cs
+++ Assets/Practice2/ObjectMoving.cs
@@ -1,8 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 namespace Practice2
 {
     public class ObjectMoving : MonoBehaviour
     {
+        [SerializeField]
+        private Animator _animator;
+
+        [SerializeField]
+        private Button _triggerButton;
+
+        private static readonly int Forward = Animator.StringToHash("Forward");
+
+        private void Awake()
+        {
+        }
+
+        public void MoveSquare()
+        {
+            _animator.SetTrigger(Forward);
+        }
     }
 }
