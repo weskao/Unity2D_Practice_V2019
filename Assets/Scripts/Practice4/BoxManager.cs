d01098263e0a949d98a4fb459f573122d6c53d65
Wes <gkm85663@gmail.com>
2020-08-21 01:35:56 +0800

    Can click button to switch to next image now

---
+++ Assets/Scripts/Practice4/BoxManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Practice4
+{
+    public class BoxManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Sprite[] _sprites;
+
+        [SerializeField]
+        private Image _controlImage;
+
+        private int _currentSpriteIndex = 0;
+
+        public void OnButtonClicked()
+        {
+            SwitchToNextImage();
+        }
+
+        private void SwitchToNextImage()
+        {
+            _currentSpriteIndex = _currentSpriteIndex % _sprites.Length;
+            _controlImage.sprite = _sprites[_currentSpriteIndex];
+
+            _currentSpriteIndex++;
+        }
+    }
+}
