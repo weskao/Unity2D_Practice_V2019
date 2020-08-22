9b0d43dc0c1d05b1a33f8bd3addc2446b7d87269
Wes <gkm85663@gmail.com>
2020-08-22 18:52:54 +0800

    Finish practice

--- Assets/Scripts/Practice4/BoxManager.cs
+++ Assets/Scripts/Practice4/BoxManager.cs
@@ -1,17 +1,48 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Practice4
 {
     public class BoxManager : MonoBehaviour
     {
+        private const int IndexToShowShockImage = 2;
+        private const float TimeToWaitShowShockImage = 0.3f;
+
         [SerializeField]
         private Sprite[] _sprites;
+
+        [SerializeField]
+        private Image _shockImage;
 
         [SerializeField]
         private Image _controlImage;
 
         private int _currentSpriteIndex = 0;
+
+        private void Update()
+        {
+            if (_shockImage != null)
+            {
+                if (_currentSpriteIndex == IndexToShowShockImage)
+                {
+                    StartCoroutine(WaitMoment());
+                }
+                else
+                {
+                    _shockImage.gameObject.SetActive(false);
+
+                    StopCoroutine(WaitMoment());
+                }
+            }
+        }
+
+        private IEnumerator WaitMoment()
+        {
+            yield return new WaitForSeconds(TimeToWaitShowShockImage);
+
+            _shockImage.gameObject.SetActive(true);
+        }
 
         public void OnButtonClicked()
         {
