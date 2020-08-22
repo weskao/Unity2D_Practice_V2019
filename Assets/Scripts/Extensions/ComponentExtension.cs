ef897e2106daf0e8b9ab47c7f10bee6b10ba6e72
Wes <gkm85663@gmail.com>
2020-08-22 19:01:09 +0800

    Refactor - Add ComponentExtension to control Unity component behavior

---
+++ Assets/Scripts/Extensions/ComponentExtension.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+    public static class ComponentExtension
+    {
+        public static void Show(this Component component)
+        {
+            component.gameObject.SetActive(true);
+        }
+
+        public static void Hide(this Component component)
+        {
+            component.gameObject.SetActive(false);
+        }
+    }
+}
