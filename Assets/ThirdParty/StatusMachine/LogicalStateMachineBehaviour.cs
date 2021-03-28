8d39650e2621fb2184e4fc86384d0174529bf921
Wes <gkm85663@gmail.com>
2021-03-29 01:14:13 +0800

    Add LogicalStateMachineBehaviour.cs and other code about it

---
+++ Assets/ThirdParty/StatusMachine/LogicalStateMachineBehaviour.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+namespace ThirdParty.StatusMachine
+{
+    // Code 來源: https://www.zhihu.com/question/40492181/answer/506143809
+    public abstract class LogicalStateMachineBehaviour : StateMachineBehaviour
+    {
+        // PRAGMA MARK - StateMachineBehaviour Lifecycle
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            this.animator = animator;
+            this.stateInfo = stateInfo;
+
+            // You can reference other monobehaviours here
+            if (this.animator != null)
+            {
+                // this.creature = animator.gameObject.GetComponent<Creature>();
+            }
+
+            this.OnStateEntered();
+            this._active = true;
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            this.animator = animator;
+            this.stateInfo = stateInfo;
+
+            this._active = false;
+            this.OnStateExited();
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            switch (animator.updateMode)
+            {
+                case AnimatorUpdateMode.AnimatePhysics:
+                    this.deltaTime = Time.fixedDeltaTime;
+                    break;
+
+                case AnimatorUpdateMode.Normal:
+                    this.deltaTime = Time.deltaTime;
+                    break;
+
+                case AnimatorUpdateMode.UnscaledTime:
+                    this.deltaTime = Time.unscaledDeltaTime;
+                    break;
+
+                default:
+                    this.deltaTime = 0f;
+                    break;
+            }
+            this.stateInfo = stateInfo;
+            this.OnStateUpdated();
+        }
+
+        // PRAGMA MARK - Internal
+        private bool _active = false;
+
+        protected Animator animator { get; private set; }
+        protected float deltaTime { get; private set; }
+
+        // TODO: You should store reference to the script that controls what this agent could do
+        //       along with any other important information.
+
+        // protected Creature creature { get; private set; }
+
+        protected AnimatorStateInfo stateInfo;
+
+        private void OnDisable()
+        {
+            if (this._active)
+            {
+                this.OnStateExited();
+            }
+        }
+
+        // Implement the following functions to write actual state behaviors
+        protected virtual void OnStateEntered() { }
+
+        protected virtual void OnStateExited()
+        {
+        }
+
+        protected virtual void OnStateUpdated()
+        {
+        }
+    }
+}
---
+++ Assets/ThirdParty/StatusMachine/PatrolBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace ThirdParty.StatusMachine
+{
+    // Code 來源: https://www.zhihu.com/question/40492181/answer/506143809
+    /// <summary>
+    /// An Example Patrol behaviour of Mechanim State Machine AI.
+    /// At this state, the agent will get an random destination and try to move towards it.
+    /// This script only updates necessary parameters for the mechanim state transition to work.
+    /// The end condition of the state should be determined from the Mechanim State Machine editor.
+    /// </summary>
+    public class PatrolBehaviour : LogicalStateMachineBehaviour
+    {
+        public float StateDurationMin = 3.0f;
+        public float StateDurationMax = 6.0f;
+        public float newDestRadius = 10.0f;
+        public Vector3 destination { get; protected set; }
+
+        protected override void OnStateEntered()
+        {
+            this.animator.SetFloat("StateDuration", Random.Range(StateDurationMin, StateDurationMax));
+            GetRandomDestination();
+        }
+
+        protected override void OnStateUpdated()
+        {
+            // Update Parameters
+            this.animator.SetFloat("StateDuration", this.animator.GetFloat("StateDuration") - this.deltaTime);
+            // this.animator.SetFloat("DistanceAway", Vector3.Distance(destination, creature.transform.position));
+
+            // Debug.DrawLine(creature.transform.position, destination, Color.yellow);
+            // creature.MoveTowards(destination, Time.fixedDeltaTime); // Replace with your movement function
+        }
+
+        protected override void OnStateExited()
+        {
+            this.animator.SetFloat("StateDuration", 0f);
+        }
+
+        private void GetRandomDestination()
+        {
+            // destination = creature.transform.position + Random.onUnitSphere * newDestRadius;
+        }
+    }
+}
