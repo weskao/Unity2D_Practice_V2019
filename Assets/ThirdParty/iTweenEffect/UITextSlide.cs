4774d673a09c3423d0642859695e63154afb70f6
Wes <gkm85663@gmail.com>
2021-04-07 01:04:50 +0800

    Add UITextSlide component

---
+++ Assets/ThirdParty/iTweenEffect/UITextSlide.cs
@@ -0,0 +1,91 @@
+﻿using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class UITextSlide : MonoBehaviour
+{
+    private enum State
+    {
+        Init,
+        Tween,
+    }
+
+    private State _currentState = State.Init;
+    private TextMeshProUGUI _tmpText = null;
+    private RectTransform _rectTransform = null;
+
+    [SerializeField]
+    private float _delayToStart = 2f;
+
+    [SerializeField]
+    private float _speed = 60f;
+
+    private void Awake()
+    {
+        _tmpText = GetComponent<TextMeshProUGUI>();
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void OnEnable()
+    {
+        _currentState = State.Init;
+        var pos = _rectTransform.anchoredPosition;
+        pos.x = -20;
+        _rectTransform.anchoredPosition = pos;
+        Invoke(nameof(CheckState), _delayToStart);
+    }
+
+    private void OnDisable()
+    {
+        iTween.Stop(gameObject);
+        CancelInvoke();
+    }
+
+    private void StateChangeToInit()
+    {
+        _currentState = State.Init;
+
+        iTween.MoveTo(gameObject, iTween.Hash("x", 0,
+                                              "time", 0,
+                                              "islocal", true,
+                                              "easetype", iTween.EaseType.linear,
+                                              "delay", 1.5,
+                                              "oncomplete", nameof(CheckState)));
+    }
+
+    private void StateChangeToTween()
+    {
+        float diff = _rectTransform.rect.width - _tmpText.preferredWidth;
+
+        Debug.Log($"diff = {diff}");
+
+        if (diff >= 0)
+            return;
+
+        _currentState = State.Tween;
+        iTween.MoveTo(gameObject, iTween.Hash("x", diff,
+                                        "delay", 1,
+                                        "speed", _speed,
+                                        "easetype", iTween.EaseType.linear,
+                                        "islocal", true,
+                                        "oncomplete", "CheckState"));
+    }
+
+    private void CheckState()
+    {
+        switch (_currentState)
+        {
+            case State.Init:
+                {
+                    StateChangeToTween();
+                }
+                break;
+
+            case State.Tween:
+                {
+                    StateChangeToInit();
+                }
+                break;
+        }
+    }
+}
