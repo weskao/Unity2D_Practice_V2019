9a235ded230d7b047ca466ded99cf4c501d1e35c
Wes <gkm85663@gmail.com>
2021-04-09 03:23:01 +0800

    Add TestRetryComponent and its example

---
+++ Assets/Scripts/Practice2/TestRetryComponent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Practice2
+{
+    public class TestRetryComponent : MonoBehaviour
+    {
+        // private bool _isEndRetry;
+        private const int MAX_RETRY_TIMES = 3;
+
+        private IEnumerator _testRetryRoutine;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                BreakRetry();
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                Init();
+
+                _testRetryRoutine = TestRetry();
+                StartCoroutine(_testRetryRoutine);
+
+                Debug.LogFormat("<color=yellow>Start TestRetry()</color>");
+                Debug.LogFormat("<color=yellow>TestRetryComponent - Start TestRetry()</color>");
+            }
+        }
+
+        private void BreakRetry()
+        {
+            // _isEndRetry = true;
+            if (_testRetryRoutine != null)
+            {
+                Debug.LogFormat("<color=yellow>TestRetryComponent - BreakRetry</color>");
+                StopCoroutine(_testRetryRoutine);
+            }
+        }
+
+        private void Init()
+        {
+            // _isEndRetry = false;
+        }
+
+        private IEnumerator TestRetry()
+        {
+            // for (var i = 1; i <= MAX_RETRY_TIMES && _isEndRetry == false; i++)
+            for (var i = 1; i <= MAX_RETRY_TIMES; i++)
+            {
+                Debug.Log($"Retry times: i = {i}");
+                yield return new WaitForSeconds(3);
+            }
+
+            Debug.LogFormat("<color=yellow>TestRetryComponent - TestRetry Done!</color>");
+        }
+    }
+}
