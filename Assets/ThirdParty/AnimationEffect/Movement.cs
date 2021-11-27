50ec9896750d9bb43532feba2acb269a5dc07e4d
Wes <gkm85663@gmail.com>
2021-11-27 17:59:38 +0800

    Add example about DOTween.

---
+++ Assets/ThirdParty/AnimationEffect/Movement.cs
@@ -0,0 +1,50 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetTransform;
+
+    [SerializeField]
+    private Button _triggerButton;
+
+    private bool _direction = false;
+
+    private void Awake()
+    {
+        _triggerButton.onClick.AddListener(OnTriggerButtonClick);
+        SetTargetTransformObject();
+    }
+
+    private void SetTargetTransformObject()
+    {
+        if (_targetTransform == null)
+        {
+            _targetTransform = transform;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _triggerButton.onClick.RemoveListener(OnTriggerButtonClick);
+    }
+
+    private void OnTriggerButtonClick()
+    {
+        Debug.LogFormat("<color=yellow>Wes - Movement - OnTriggerButtonClick()</color>");
+
+        Debug.Log($"Wes - _direction = {_direction}");
+        if (_direction)
+        {
+            _targetTransform.DORewind();
+        }
+        else
+        {
+            _targetTransform.DOMoveX(0.5f, 1).From();
+        }
+
+        _direction = !_direction;
+    }
+}
